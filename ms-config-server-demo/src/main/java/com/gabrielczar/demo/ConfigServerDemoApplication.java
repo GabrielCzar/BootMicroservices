41a241e51e85238d8ea6b5fa13f16dfab75c9947
Gabriel César <gcal1@live.com>
2018-05-13 17:48:53 -0300

    up

--- ms-config-server-demo/src/main/java/com/gabrielczar/demo/ConfigServerDemoApplication.java
+++ ms-config-server-demo/src/main/java/com/gabrielczar/demo/ConfigServerDemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerDemoApplication.class, args);
     }
--- ms-demo-client/src/test/java/com/gabrielczar/msdemoclient/MsDemoClientApplicationTests.java
+++ ms-demo-client/src/test/java/com/gabrielczar/msdemoclient/MsDemoClientApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gabrielczar.demo;
+package com.gabrielczar.msdemoclient;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class MsDemoClientApplicationTests {
 
     @Test
     public void contextLoads() {
