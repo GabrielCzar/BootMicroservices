6f8c2bab50815378042101be7fe99d26b2a70ace
Gabriel César <gcal1@live.com>
2018-05-13 21:29:56 -0300

    up

--- ms-demo-client/src/main/java/com/gabrielczar/msdemoclient/MsDemoClientApplication.java
+++ ms-demo-client/src/main/java/com/gabrielczar/msdemoclient/MsDemoClientApplication.java
@@ -1,13 +1,16 @@
 package com.gabrielczar.msdemoclient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableHystrix
 @EnableEurekaClient
 @SpringBootApplication
 public class MsDemoClientApplication {
@@ -23,8 +26,14 @@ class MessageRestController {
     @Value("${message: Hello Default}")
     private String message;
 
+
+    @HystrixCommand(fallbackMethod = "getMessageFallback")
     @GetMapping("/message")
     String getMessage() {
         return message;
+    }
+
+    private String getMessageFallback(){
+        return "Hello Fallback";
     }
 }
